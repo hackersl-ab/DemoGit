ffc20748c4a0c67604551261ae70e47b02d8be17
Pranav Merugu <Pranav@Pranavs-MacBook-Pro.local>
2020-09-02 22:18:56 -0400

    Flask Hello

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
-print("Hello World")
-for i in range(11):
-  print(i)
-print("You have succesfully implemented git")
-print("Test")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
